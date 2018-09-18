fd87b4a79b3a73de4bfca922ac2c4f3ade5a7caf
iiFireGamer <42865927+iiFireGamer@users.noreply.github.com>
2018-09-19 00:10:50 +0200

    Update helpinfo.js

--- help/helpinfo.js
+++ help/helpinfo.js
@@ -8,21 +8,21 @@ message.author.send(`\n\n\`\`\`
                📚 fun commands 👏    
 _________________________________________________
 |                                               |
-| -bots to show all bots in server              |
-| -rooms to show all rooms in server            |
-| -count start count time                       |
-| -meteo To see the weather                     |
-| -invite invitation the bot                    |
-| -bcowner send message to owner                |
-| -serverpic show photo the server              |
-| -userinfo Information about user              |
-| -server Information about server              |
-| -avatar photo profile person                  |
-| -ping The quality of the bot connection       |
-| -myid info about your profile                 |
-| -botinfo Information about bot                |
-| -support link server support                  |
-| -uptime boot time the bot                     |
+| f!bots to show all bots in server              |
+| f!rooms to show all rooms in server            |
+| f!count start count time                       |
+| f!meteo To see the weather                     |
+| f!invite invitation the bot                    |
+| f!bcowner send message to owner                |
+| f!serverpic show photo the server              |
+| f!userinfo Information about user              |
+| f!server Information about server              |
+| f!avatar photo profile person                  |
+| f!ping The quality of the bot connection       |
+| f!myid info about your profile                 |
+| f!botinfo Information about bot                |
+| f!support link server support                  |
+| f!uptime boot time the bot                     |
 |_______________________________________________|
                                              
 we will add more commands a soon to contact the owner
