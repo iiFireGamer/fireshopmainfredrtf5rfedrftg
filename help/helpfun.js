d2cfe7e349c6ff5d7f634a67af9d7e344f685dc1
iiFireGamer <42865927+iiFireGamer@users.noreply.github.com>
2018-09-19 00:09:46 +0200

    Update helpfun.js

--- help/helpfun.js
+++ help/helpfun.js
@@ -8,28 +8,28 @@ message.author.send(`\n\n\`\`\`
               🎃 fun commands 👏    
 _________________________________________________
 |                                               |
-|  -dog <@user> Show random dogs                |
-|  -cat Show random cats                        |
-|  -catgif Show random cats                     |
-|  -fox Show random fox                         |
-|  -hug <@user> Hug somebody                    |
-|  -punch <@user> Punch for something           |
-|  -kill <@user> kill someone                   |
-|  -anime show random anime photos              |
-|  -memes show random photos                    |
-|  -garo to get sigart                          |
-|  -rabbits show random rabbits photos          |
-|  -slap <user> To slap a person                |
-|  -poke <user> To poke a person                |
-|  -cuddle <user> To cuddle a person            |
-|  -pat <user> to pat a person                  |
-|  -kiss <user> to kiss a person                |
-|  -tickle <user> to tickle a person            |
-|  -ask to ask bot anything answer you          |
-|  -otaku to show random photos otaku           |
-|  -any                                         |
-|  -gay To find out who someone is gay          |
-|  -nik to nick any one xD                      |
+|  f!dog <@user> Show random dogs                |
+|  f!cat Show random cats                        |
+|  f!catgif Show random cats                     |
+|  f!fox Show random fox                         |
+|  f!hug <@user> Hug somebody                    |
+|  f!punch <@user> Punch for something           |
+|  f!kill <@user> kill someone                   |
+|  f!anime show random anime photos              |
+|  f!memes show random photos                    |
+|  f!garo to get sigart                          |
+|  f!rabbits show random rabbits photos          |
+|  f!slap <user> To slap a person                |
+|  f!poke <user> To poke a person                |
+|  f!cuddle <user> To cuddle a person            |
+|  f!pat <user> to pat a person                  |
+|  f!kiss <user> to kiss a person                |
+|  f!tickle <user> to tickle a person            |
+|  f!ask to ask bot anything answer you          |
+|  f!otaku to show random photos otaku           |
+|  f!any                                         |
+|  f!gay To find out who someone is gay          |
+|  f!nik to nick any one xD                      |
 |_______________________________________________|
                                              
 we will add more commands a soon to contact the owner
