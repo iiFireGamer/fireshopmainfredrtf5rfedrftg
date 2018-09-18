49d3b91f7e93f6496e265703a1693b85b04f5351
iiFireGamer <42865927+iiFireGamer@users.noreply.github.com>
2018-09-19 00:07:22 +0200

    Update adminar.js

--- help/adminar.js
+++ help/adminar.js
@@ -8,22 +8,22 @@ message.author.send(`\n\n\`\`\`fix
                  اوامر المسؤولين                          
 __________________________________________________
 |                                                 
-|  -ban <@user> <reason> حظر المستخدم           
-|  -kick <@user> <reason> ركل المستخدم           
-|  -delete chat مسح الشات                        
-|  -mute <@user> <time> <reason> اسكات المستخدم  
-|  -unmute <@user> <reason> الغاء اسكات المستخدم 
-|  -role <@user> <namerole> اعطاء دور            
-|  -removerole <@user> <name role> ازالة ادور    
-|  -bans جميع أعضاء المحظورة                     
-|  -chatmute اغلاق الشات                          
-|  -openchat فتح الشات                           
-|  -say امر ساي عادي                             
-|  -sayembed امر ساي مع المبيد                   
-|  -shut @<user> اسكات شخص من روم محدد           
-|  -open @<user> الغاء اسكات شخص                
-|  -antilinks منع روابط السيرفرات الاخرى         
-|  -antiinfo معلومات حول مانع روابط         
+|  f!ban <@user> <reason> حظر المستخدم           
+|  f!kick <@user> <reason> ركل المستخدم           
+|  f!delete chat مسح الشات                        
+|  f!mute <@user> <time> <reason> اسكات المستخدم  
+|  f!unmute <@user> <reason> الغاء اسكات المستخدم 
+|  f!role <@user> <namerole> اعطاء دور            
+|  f!removerole <@user> <name role> ازالة ادور    
+|  f!bans جميع أعضاء المحظورة                     
+|  f!chatmute اغلاق الشات                          
+|  f!openchat فتح الشات                           
+|  f!say امر ساي عادي                             
+|  f!sayembed امر ساي مع المبيد                   
+|  f!shut @<user> اسكات شخص من روم محدد           
+|  f!open @<user> الغاء اسكات شخص                
+|  f!antilinks منع روابط السيرفرات الاخرى         
+|  f!antiinfo معلومات حول مانع روابط         
 |________________________________________________
 
 لإرسال التقارير اليها logs لا تنسى ان تنشأ قناة اسمها   
