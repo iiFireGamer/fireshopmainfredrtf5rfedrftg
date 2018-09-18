8ab228929fcdf0b3bc677695b3d89aec9796a685
iiFireGamer <42865927+iiFireGamer@users.noreply.github.com>
2018-09-19 00:46:25 +0200

    Update owner.js

--- help/owner.js
+++ help/owner.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 exports.run = (client, message, args) => {
   if(!message.channel.guild) return message.reply('this command is for guilds only!');
-  if (message.author.id !== '406192153979518976') return message.reply('sorry but this command for owner 😪')
+  if (message.author.id !== '408374252291751976') return message.reply('sorry but this command for owner 😪')
   message.author.send(`\n\n\`\`\`autohotkey
      ⚠ commands controle a bot 💬
 ________________________________________________________________
